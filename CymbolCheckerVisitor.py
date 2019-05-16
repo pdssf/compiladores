0e94273277df66050c40630c386673cd92d3df3a
AdrianoSantana <45883008+AdrianoSantana@users.noreply.github.com>
2019-05-16 18:02:20 -0300

    Add files via upload
    Criando AddSubExpr funcionando para o caso em que as duas são constantes, e falta uma linha de print para uma expressão entre uma constante e um variável.

--- CymbolCheckerVisitor.py
+++ CymbolCheckerVisitor.py
@@ -3,12 +3,14 @@ from autogen.CymbolParser import CymbolParser
 from autogen.CymbolVisitor import CymbolVisitor
 
 funcoes = {}
-#dicionário de variáveis
+#dicionário de nro variáveis --- %i
 variaveis = {}
 #dicionário de parâmetros
 parametros = {}
 #dicionario de variaveis globais
 globais = {}
+#dicionario de valores variaveis
+valores_variaveis = {}
 
 class Type:
 	INT = "int"
@@ -19,13 +21,16 @@ class CymbolCheckerVisitor(CymbolVisitor):
 	def __init__(self):
 		self.estouDentro_daFuncao = 0
 		self.count = 0
+		self.nome_func_atual = None
 		
 	def visitIntExpr(self, ctx:CymbolParser.IntExprContext):
-		print('ret i32')
-		return Type.INT
+		return int(ctx.INT().getText())
+
+	def visitNotExpr(self, ctx:CymbolParser.NotExprContext):
+		return self.visitChildren(ctx)
 
 	def visitFloatExpr(self, ctx:CymbolParser.FloatExprContext):
-		return Type.FLOAT
+		return float(ctx.FLOAT().getText())
 
 	def visitFormTypeBoolean(self, ctx:CymbolParser.BooleanExprContext):
 		return Type.BOOL
@@ -34,6 +39,7 @@ class CymbolCheckerVisitor(CymbolVisitor):
 		var_name = ctx.ID().getText()
 		tyype = ctx.tyype().getText()
 		expr = ctx.expr()
+		func_name = self.nome_func_atual
 
 		if(self.estouDentro_daFuncao == 0):
 			globais[var_name] = tyype #salvo a variavel e seu tipo
@@ -61,20 +67,32 @@ class CymbolCheckerVisitor(CymbolVisitor):
 			self.count += 1
 			if(tyype == 'int'):
 				print('%' + str(self.count) +  '= alloca i32, align 4')
+				variaveis[func_name,var_name] = self.count
 			elif(tyype == 'float'):
 				print('%' + str(self.count) +  '= alloca float, align 4')
+				variaveis[func_name,var_name] = self.count
 			else:
 				print('%' + str(self.count) + '= alloca i1, align 4')
+				variaveis[func_name,var_name] = self.count
 
 			if (expr != None):
-				print('store', end = ' ')
 				if(tyype == 'int'):
-					print('i32 ' + expr.getText() + ', i32* %'+ str(self.count), end='')					
+					if(('+' in expr.getText()) or ('-' in expr.getText())):
+						self.visitChildren(ctx)
+					else:	
+						print('store i32 ' + expr.getText() + ', i32* %'+ str(self.count) + ', align 4')
+						valores_variaveis[func_name,var_name] = int(expr.getText())
+						#print('Nome da func :' + str(func_name) + 'ID da Variavel :' + var_name + 'Valor :', int(expr.getText()))
 				elif(tyype == 'float'):
-					print('float ' + expr.getText(), ', float* %'+str(self.count), end='')
+					if(('+' in expr.getText()) or ('-' in expr.getText())):
+    						self.visitChildren(ctx)
+					else:
+						valores_variaveis[func_name,var_name] = float(expr.getText())
+						print('store float ' + expr.getText(), ', float* %'+str(self.count) + ', align 4')
 				else:
-					print('i1 '+ expr.getText(), ', i1* %'+str(self.count), end='')
-				print(', align 4')
+					valores_variaveis[func_name,var_name] = str(expr.getText())
+					print('store i1 '+ expr.getText(), ', i1* %'+str(self.count) + ', align 4')
+				
 			  		
 	def visitFuncDecl(self, ctx:CymbolParser.FuncDeclContext):
 		#elementos iniciais: nome da funcao, tipo, e lista de parametros
@@ -118,6 +136,7 @@ class CymbolCheckerVisitor(CymbolVisitor):
 				if ('int' in p.getText()):
 					print('%' + str(self.count) + ' = alloca i32, align 4')
 					variaveis[func_name,p.ID().getText()] = self.count
+					#print('Nome func :' + func_name + 'ID :' + p.ID().getText())
 				elif('float' in p.getText()):
 					print('%' + str(self.count) + ' = alloca float, align 4')
 					variaveis[func_name,p.ID().getText()] = self.count
@@ -135,19 +154,71 @@ class CymbolCheckerVisitor(CymbolVisitor):
 					print('store float %' + str(j) + ', float* %'+ str(i) + ', align 4')
 				else:
 					print('store i1 %' + str(j) + ', i1* %'+ str(i) + ', align 4')
+
 				j += 1
 				i += 1
 		else:
 			print(') #0 {')
 
 		self.estouDentro_daFuncao= 999
+		self.nome_func_atual = func_name
 		self.visitChildren(ctx)
 		print('}') #fim da funçao
 
 	def visitAddSubExpr(self, ctx:CymbolParser.AddSubExprContext):
 		left = ctx.expr()[0].accept(self)
 		right = ctx.expr()[1].accept(self)
-		return 0
+		nome_func = self.nome_func_atual
+		#print('nome func ' + str(nome_func))
+		exprOperador = ctx.op.text
+		if ('+' == exprOperador):
+			#Caso 1 : Duas constantes
+			if((left != None) and (right != None)):
+				result = left + right
+				print('store i32 ' + str(result) + ', i32* %'+ str(self.count)+ ', align 4')
+			
+			#Caso 2 : Um dos valores é constante o outro é de uma variavel
+			#define i32 @soma() #0
+ 			# %1 = alloca i32, align 4
+  			#%2 = alloca i32, align 4
+  			#store i32 10, i32* %1, align 4
+ 			#%3 = load i32, i32* %1, align 4
+  			#%4 = add nsw i32 %3, 150
+ 			#store i32 %4, i32* %2, align 4 --- Falta esse
+			#ret i32 1500
+
+			elif((left == None) or (right == None)):
+				# se um dos operandos for uma variável,left ou right estara com seu valor NONE
+				# Logo é preciso saber o nome da variável para buscar o seu valor na estrutura de 
+				# dicionario ja criada valores_variaveis[ctx.expr().getText(),str(ctx.expr())]
+				if(left == None):
+					self.count += 1
+					exprIDLEFT = str(ctx.expr()[0].ID())
+					nro_variavel = variaveis[nome_func,exprIDLEFT]
+					result = valores_variaveis[str(nome_func),str(exprIDLEFT)] + right
+					print('%' + str(self.count) + '= load i32, i32* %' + str(nro_variavel) + ', align 4')
+					#print('nome variable  :' + str(nro_variavel))
+					self.count += 1
+					print('%' + str(self.count) + '= add nsw i32 %' + str(self.count - 1) + ',' + str(result))
+					#print('store i32 %' +str(self.))
+				else:
+					self.count += 1
+					exprIDRIGHT = str(ctx.expr()[1].ID())
+					nro_variavel = variaveis[nome_func,exprIDRIGHT]
+					result = valores_variaveis[str(nome_func),str(exprIDRIGHT)] + left
+					print('%' + str(self.count) + '= load i32, i32* %' + str(nro_variavel) + ', align 4')
+					#print('nome variable  :' + str(nro_variavel))
+					self.count += 1
+					print('%' + str(self.count) + '= add nsw i32 %' + str(self.count - 1) + ',' + str(result))
+					#print('store i32 %' +str(self.))
+				#Caso 3 : Os dois valores são variáveis
+				#else:
+		else:
+			result = left - right
+			print('store i32 ' + str(result) + ', i32* %'+ str(self.count)+ ', align 4')
+
+		#print('toma aqui teu resultado', result)
+		return self.visitChildren(ctx)
 
 	def aggregateResult(self, aggregate:Type, next_result:Type):
 		return next_result if next_result != None else aggregate
@@ -161,3 +232,9 @@ class CymbolCheckerVisitor(CymbolVisitor):
 		print('ret i32' + str(self.count))
 		#return self.visitChildren(ctx)'''
 	#clang -S -emit-llvm test_clang.c
+ 
+ #not
+ #and e or
+ #igual e diferente
+ #Maior e menor
+ #>= <=
